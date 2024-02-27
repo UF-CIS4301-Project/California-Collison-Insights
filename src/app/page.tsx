d2d7339e7b8a8635d16ad08940fc63ff45a94320
spwalgren <spencerwalgren@LAPTOP-KB6NE80R>
2024-02-27 13:02:52 -0500

    Initial commit. Basic welcome bar.

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+function IndexPage() {
+  return (
+    <div>
+      <header className="bg-[#20146a] px-12 h-20">
+        <nav className="flex items-center justify-between">
+          <div></div>
+          <div className="text-2xl font-bold text-white">
+            California Collision Insights
+          </div>
+          <div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default IndexPage
