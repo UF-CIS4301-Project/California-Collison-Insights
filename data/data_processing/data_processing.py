552ffae99b3c6200931c98fbf5cf7d37470221e3
Jack Wang <39379864+jackwcodes@users.noreply.github.com>
2024-02-29 21:39:26 -0800

    add count for NULL values

--- data/data_processing/data_processing.py
+++ data/data_processing/data_processing.py
@@ -2,7 +2,7 @@ import os  # for writing length outputs
 import sqlite3
 
 
-def find_longest_string_lengths(output_path, database_path, database_table, columns):
+def find_attributes(output_path, database_path, database_table, columns):
     # Connect to SQLite database
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
@@ -12,22 +12,23 @@ def find_longest_string_lengths(output_path, database_path, database_table, colu
         print(f"-----------------Outputting values for table: {database_table}-----------------")
         file.write(f"--------------- Longest string lengths from {database_table} --------------\n")
 
-        # Iterate over the selected columns and find the longest string in each
+        # Iterate over the selected columns and find the longest string in each and how many NULLs each column contains
         for column in columns:
             # Construct the SELECT query to find the maximum length of strings in the current column
-            query = f"SELECT MAX(LENGTH({column})) FROM {database_table}"
-
-            # Execute the query
-            cursor.execute(query)
-
-            # Fetch the result
+            query_max_string_length = f"SELECT MAX(LENGTH({column})) FROM {database_table}"
+            cursor.execute(query_max_string_length)
             longest_string_length = cursor.fetchone()[0]
 
+            # Construct the SELECT query to count the number of NULL values in the current column
+            query_null_value_count = f"SELECT COUNT(*) FROM {database_table} WHERE {column} IS NULL"
+            cursor.execute(query_null_value_count)
+            null_value_count = cursor.fetchone()[0]
+
             # Print the result
-            print("Longest string length in", column, ":", longest_string_length)
+            print(f"Column: {column} | Longest String: {longest_string_length} | Number of nulls: {null_value_count}\n")
 
             # Write the result to the output file
-            file.write("{} : {}\n".format(column, longest_string_length))
+            file.write(f"Column: {column} | Longest String: {longest_string_length} | Number of nulls: {null_value_count}\n")
 
         file.write('\n')  # newline at end of file
 
@@ -35,7 +36,7 @@ def find_longest_string_lengths(output_path, database_path, database_table, colu
     conn.close()
 
 
-def string_lengths():
+def column_attributes():
     # output data file path, make the output directory if it doesn't exist
     output_dir = 'output'
     result_file_path = os.path.join(output_dir, 'output.txt')
@@ -53,20 +54,20 @@ def string_lengths():
     selected_columns = ["collision_severity", "pcf_violation_category", "county_location", "primary_road",
                         "statewide_vehicle_type_at_fault", "weather_1", "weather_2", "road_surface", "road_condition_1",
                         "road_condition_2", "lighting"]
-    find_longest_string_lengths(result_file_path, traffic_data_path, selected_table, selected_columns)
+    find_attributes(result_file_path, traffic_data_path, selected_table, selected_columns)
 
     selected_table = 'parties'
     selected_columns = ["vehicle_make", "party_race", "party_sex"]
-    find_longest_string_lengths(result_file_path, traffic_data_path, selected_table, selected_columns)
+    find_attributes(result_file_path, traffic_data_path, selected_table, selected_columns)
 
     selected_table = 'victims'
     selected_columns = ['victim_degree_of_injury']
-    find_longest_string_lengths(result_file_path, traffic_data_path, selected_table, selected_columns)
+    find_attributes(result_file_path, traffic_data_path, selected_table, selected_columns)
 
 
 # main function of code
 def main():
-    string_lengths()
+    column_attributes()
 
 
 # this code is only meant to be executed from here, nowhere else
