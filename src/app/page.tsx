437695ac4e66d3c9db11795d115ad14c5d7b8b1a
spwalgren <spencerwalgren@LAPTOP-KB6NE80R>
2024-04-03 14:51:38 -0400

    Added basic request for num of tuples in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
+'use client';
+
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import { Card, CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
 
 
 function IndexPage() {
   const datasets = [{ "key": "one", "value": "random" }, { "key": "two", "value": "random" }, { "key": "three", "value": "random" }, { "key": "four", "value": "random" },]
+
+  let numTuples: number = 0
+  const [selectedTuples, setSelectedTuples] = useState(numTuples)
+
   return (
     <div>
-      <div className="w-full h-screen flex flex-row justify-center items-center text-center">
+      <div className="w-full h-full flex flex-row justify-center items-center text-center">
         <div className="flex flex-col text-xl m-20 space-y-12">
-          <div className="p-10 text-justify rounded shadow-lg bg-white">Welcome Spiel - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, et sint nulla beatae non eveniet accusantium ratione velit corrupti labore odio quam voluptatum recusandae culpa quae nesciunt corporis. Architecto, asperiores? Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quaerat illum eveniet deleniti fugiat! Adipisci, voluptate praesentium ut id dolor ipsam ullam expedita in mollitia eligendi enim ducimus, porro cupiditate.</div>
+          <div className="p-10 text-justify rounded shadow-lg bg-white">Welcome Spiel - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, et sint nulla beatae non eveniet accusantium ratione velit corrupti labore odio quam voluptatum recusandae culpa quae nesciunt corporis. Architecto, asperiores? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br/>
+          <br/>
+          Click <button>here</button> for total number of tuples in database: {selectedTuples}
+          </div>
           <div className="my-10 text-3xl">Feel free to explore the datasets we used</div>
 
         {/* Box components which describe and link to each of our datasets - currently just holds dummy data/text  */}
